46b08c7a1c18613bd6d852b3c31ffc2203c202e1
Cnexy <gr8cnexy@gmail.com>
2022-10-20 13:05:02 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_numbers - print 0123456789
+* print_numbers - print 0-9
 * made by Cnexy
 * Return: void
 */
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
